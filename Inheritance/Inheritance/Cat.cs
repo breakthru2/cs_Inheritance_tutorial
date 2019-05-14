d6746b434dadc3aa1a5ff42d234241e7f88a6f27
Paul Breakthrough <paulbreakthru@outlook.com>
2019-05-14 15:02:02 +0100

    Classes Created
    Animal Class, Dog Class and Cat Class created.

---
+++ Inheritance/Inheritance/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("A cat meows");
+        }
+    }
+}
--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("All animals eat");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
